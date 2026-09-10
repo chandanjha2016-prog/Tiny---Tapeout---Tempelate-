305f99db20bac4e03bbf5449814780a88a239cff
chandanjha2016-prog <chandanjha2016@gmail.com>
2026-09-10 09:12:19 +0530

    Refactor safety module with updated inputs and outputs

--- src/project.v
+++ src/project.v
@@ -1,9 +1,23 @@
-
-module safety_chip_tt (
-    input [7:0] io_in,
-    output [7:0] io_out
+`default_nettype none
+module tt_um_chandanjha_safety (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
-wire sos = io_in[0];
-wire fall = io_in[1];
-wire gas = io_in[2];
-wire danger = sos | fall | gas;
+  wire sos = ui_in[0];
+  wire fall = ui_in[1];
+  wire gas = ui_in[2];
+  wire danger = sos | fall | gas;
+  assign uo_out[0] = danger;
+  assign uo_out[1] = danger;
+  assign uo_out[2] = fall;
+  assign uo_out[7:3] = 0;
+  assign uio_out = 0;
+  assign uio_oe = 0;
+  wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
+endmodule
